64868541cadacb8ddcd28eb2d04fd821a5ff1ea1
abner4g <abner.marques89@gmail.com>
2023-05-20 22:53:35 -0300

    commit

--- src/main/java/br/com/spring/springBoot/controller/myController.java
+++ src/main/java/br/com/spring/springBoot/controller/myController.java
@@ -1,5 +1,6 @@
 package br.com.spring.springBoot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class myController {
 
+    @GetMapping("/resource")
     public String getResource(@RequestParam("name") String name){
         return "hello, " + name + "! This is the resource";
     }
